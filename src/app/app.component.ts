f98184390a96ace50bb62861fd58a12bf66064f4
José Emilio <gutiermoran@gmail.com>
2024-03-06 20:20:28 +0100

    A first implementation of the header and footer has been made.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'joseemiliogutierrez';
+
+  currentDate = new Date();
+
+  title = 'gutiermoran';
 }
