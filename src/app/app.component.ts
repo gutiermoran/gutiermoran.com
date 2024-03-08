125786a9d3c0c74d4e85fd0e6ee44686d7787b59
José Emilio Gutiérrez <hi@gutiermoran.com>
2024-03-08 04:53:27 +0100

    The avatar has been resized. The background colour of the social bar is blue now. CV button has been added. The name and occupation font has been changed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,13 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
-
 import { LanguageSelectorComponent } from "./components/language-selector/language-selector.component";
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, DatePipe, TranslateModule, LanguageSelectorComponent],
+  imports: [RouterOutlet, RouterLink, DatePipe, TranslateModule, LanguageSelectorComponent, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
