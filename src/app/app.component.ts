b379997ce4f21bc7d5ceed3e07ba6351806103a5
José Emilio Gutiérrez <hi@gutiermoran.com>
2024-03-08 20:48:14 +0100

    The CV button has been modified. The button to copy the email has been added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,23 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 
 import { ButtonModule } from 'primeng/button';
 
+import { ClipboardModule } from 'ngx-clipboard';
+import { ClipboardService } from 'ngx-clipboard'
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, DatePipe, ButtonModule],
+  imports: [RouterOutlet, RouterLink, DatePipe, ButtonModule, ClipboardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   currentDate = new Date();
+  private email: string = "jose@gutiermoran.com";
+
+  constructor(private readonly clipboardService: ClipboardService) { }
+
+  copy() {
+    this.clipboardService.copy(this.email);
+  }
 }
