84554db188ea0066ef4fde7def05431a5221f81e
José Emilio Gutiérrez <hi@gutiermoran.com>
2024-03-14 02:22:19 +0100

    Work 🚧

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 
 import { MainProjectsComponent } from '../../modules/projects/components/main-projects/main-projects.component';
-import { MainSkillsComponent } from '../../modules/skills/components/main-skills/main-skills.component';
 import { MainExperienceComponent } from '../../modules/experience/components/main-experience/main-experience.component';
 
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [MainProjectsComponent, MainSkillsComponent, MainExperienceComponent],
+  imports: [MainProjectsComponent, MainExperienceComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
